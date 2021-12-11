772cab8c97277f16a40c4429b703adf2a5b23c04
Marlon Rogério <msrogerio9@gmail.com>
2021-12-11 03:08:30 -0500

    implantacao do autômato

--- ling_nasser.py
+++ ling_nasser.py
@@ -3,6 +3,13 @@ from datetime import datetime
 
 var = {}
 linhas = 0
+contador_de_erros = 0
+controle_erros = []
+
+def add_erro(str):
+    global contador_de_erros
+    contador_de_erros += 1
+    controle_erros.append(str)
 
 def add_linhas(add=0):
     global linhas
@@ -11,7 +18,6 @@ def add_linhas(add=0):
         return linhas
     linhas += 1
     return linhas
-
 
 def armazena_memoria(chave, valor):
     var.update({chave: valor})
@@ -24,10 +30,50 @@ def help():
         '-i [--int] - declara variáveis inteiras \n' + 
         '\tEx.: -i x=10, y=20 or --int x=10\n' + 
         '-op [--operacao] - executa operacao atitimática simples entre dois números\n' +
-        '\tEx.-op x-y or -op x + y\n' + 
+        '\tEx.-op 1-2 or -op 1 + 2\n' + 
+        '-a [--aceitacao] - verifica aceitação da expressaão\n' +
+        '\tEx.-a x-y or -a x + y\n' + 
         '-x [--exit] - sair do console\n'
     )
     add_linhas(8)
+
+def verifica_expressao(exp):
+    """
+    E = {q0, q1, q2}
+    Σ = {t, +, -}
+    t ∈ N
+    δ:
+        q0 <- t -> q1
+        q0 <- + | - -> q2
+        q1 <- t -> q2
+        q1 <- - | + -> q0
+        q2 <- - | + | t -> q2
+    i = q0
+    F = q0
+    Γ = {x} 
+    """
+    exp = exp.split(None, 1)
+    exp = exp[1].replace(' ', '')
+    E = ['q0', 'q1', 'q2']
+    i = E[0]
+    F = E[1]
+    e_atual = i
+    for t in exp:
+        if e_atual == E[0] and t.isdigit():
+            e_atual = E[1]
+        elif e_atual == E[0] and t=='+' or t=='-':
+            e_atual = E[2]
+        elif e_atual == E[1] and t.isdigit():
+            e_atual = E[2]
+        elif e_atual == E[1] and t=='+' or t=='-':
+            e_atual = E[0]
+        elif e_atual == E[2] and t=='+' or t=='-' or t.isdigit():
+            e_atual = E[2]
+    if e_atual == F:
+        print('<%d>..: A expressão %s é aceita pelo autômato ' % (add_linhas(), exp))
+        return True
+    print('<%d>..: A expressão %s não é aceita pelo autômato ' % (add_linhas(), exp))
+    return False
 
 def mostra(retorno):
     linha = linhas
@@ -35,10 +81,13 @@ def mostra(retorno):
     if '"' in retorno:
         retorno = retorno.replace('"', '')
         print('<%d>..: %s ' % (add_linhas(), retorno))
+        add_erro('[Erro linha %d]' % linha)
     elif retorno in var:
         print('<%d>..: %s' % (add_linhas(), var[retorno]))
+        add_erro('[Erro linha %d]' % linha)
     elif not retorno in var:
         print('<%d>..:[Erro na linha %d] %s não definido' % (add_linhas(), linha, retorno))
+        add_erro('[Erro linha %d]' % linha)
 
 def limpa():
     global var
@@ -57,20 +106,24 @@ def inteiro(opcao):
         for a in opcao:
             if not '=' in a:
                 print('<%d>..:[Erro na linha %d] O operador "=" não foi informado' % (add_linhas(), linha))
+                add_erro('[Erro linha %d]' % linha)
                 return False
             temp = a.split('=')
             if not temp[1].isdigit():
                 print('<%d>..:[Erro na linha %d] Valor inteiro não informado' % (add_linhas(), linha))
+                add_erro('[Erro linha %d]' % linha)
                 return False
             armazena_memoria(temp[0], int(temp[1]))
         return True
     else:
         if not '=' in opcao:
             print('<%d>..:[Erro na linha %d] O operador "=" não foi informado' % (add_linhas(), linha))
+            add_erro('[Erro linha %d]' % linha)
             return False
         temp = opcao.split('=')
         if not temp[1].isdigit():
             print('<%d>..:[Erro na linha %d] Valor inteiro não informado' % (add_linhas(), linha))
+            add_erro('[Erro linha %d]' % linha)
             return False
         armazena_memoria(temp[0], int(temp[1]))
         return True
@@ -83,17 +136,19 @@ def operacao(opcao):
         operacao = opcao.split('+')
         if not operacao[0] in var or not operacao[1] in var:
             print('<%d>..:[Erro na linha %d] Valor não encontrado' % (add_linhas(), linha))
+            add_erro('[Erro linha %d]' % linha)
             return False
         operacao = var[operacao[0]] + var[operacao[1]]
-        print(operacao)
+        print('<%d>..: %s' % (add_linhas() ,operacao))
         return True
     if '-' in opcao:
         operacao = opcao.split('-')
         if not operacao[0] in var or not operacao[1] in var:
             print('<%d>..:[Erro na linha %d] Valor não encontrado' % (add_linhas(), linha))
+            add_erro('[Erro linha %d]' % linha)
             return False
         operacao = var[operacao[0]] - var[operacao[1]]
-        print(operacao)
+        print('<%d>..: %s' % (add_linhas() ,operacao))
         return True
 
 hoje = datetime.utcnow()
@@ -125,9 +180,18 @@ while(saida!=0):
         
     elif '-x' in opcao or '--exit' in opcao:
         saida=0
+
+    elif '-a' in opcao or '--aceitacao' in opcao:
+        verifica_expressao(opcao)
     
     else:
-        print('..:[Erro] %d Escolha uma opção válida.' % linhas)
+        print('<%d>..: [Erro na linha %d] Escolha uma opção válida.' % (add_linhas(),linhas))
+        add_erro('[Erro linha %d]' % linhas)
         help()
 
-print('Saindo do console...')    
+for a in controle_erros:
+
+    print('<%d>..: %s' % (add_linhas(), a))
+
+print('<%d>..: Total de erros: %d' % (add_linhas(), contador_de_erros))
+print('<%d>..: Saindo do console...' % add_linhas())
